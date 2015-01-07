e998302a23c340831db3f044cc94f21aa66e327a
Rajah Chacko <4jtszmnuifr@gmail.com>
2015-01-07 14:26:04 -0500

    Initial commit

---
+++ src/main/java/com/sandbox/HelloWorld/HelloService.java
@@ -0,0 +1,12 @@
+package com.sandbox.HelloWorld;
+
+import org.springframework.stereotype.Component;
+/**
+ * This service simply modify the original message and return the new message
+ */
+@Component
+public class HelloService {
+    public String sayHello(String name) {
+        return "Hello, " + name + "!";
+    }
+}
---
+++ src/main/java/com/sandbox/HelloWorld/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by a137078 on 1/7/2015.
+ */
+package com.sandbox.HelloWorld;
+
+// the basics for this Hello World are from http://javaee.ch/category/intellij/.
